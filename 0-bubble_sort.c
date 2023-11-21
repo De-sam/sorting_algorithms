f1788a34e31749eb65ce9fb58383307d7773598e
De-sam <samsoncoded@gmail.com>
2023-11-21 10:36:49 +0100

    Sorting alogorithms task 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include "sort.h"
-/**
- * bubble_sort - This function sorts an array of numbers using bubble sort
- * in acsending order
- * @array: The unsorted array
- * @size: The size of the array
- */
 
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
+	int swapped;
 
+	if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
+
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -23,7 +27,11 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+				swapped = 1;
 			}
 		}
+
+		if (swapped == 0)
+			break;
 	}
 }
